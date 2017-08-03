8c210aba7415923000581eb64bf42f67e7d8091e
Yuvaraj S <s.yuvaraj03@gmail.com>
2017-08-03 20:26:21 +0530

    states

--- my-app/src/Clock.js
+++ my-app/src/Clock.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
 class Clock extends React.Component{
 
+  constructor(props){
+    super(props);
+    this.state = {
+      date: new Date()
+    };
+  }
 
-
-  render(){  
-    return(
-       <div>
-           <h1>It is {new Date().toLocaleTimeString()} </h1>
-        </div>
-   
+  componentDidMount(){
+     this.timerID = setInterval(
+      () => this.tick(),
+      1000
     );
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+  
+  render(){
+    return(
+      <div>
+        <h1>Hi, there</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div> 
+    );   
   }
 }
 
