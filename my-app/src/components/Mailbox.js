38bce2a6f514a4f697885a59786fd4b807712b36
Yuvaraj S <s.yuvaraj03@gmail.com>
2017-08-07 19:16:08 +0530

    inline if with logical &&

--- my-app/src/components/Mailbox.js
+++ my-app/src/components/Mailbox.js
@@ -4,17 +4,17 @@ class MailBox extends React.Component{
 
 	constructor(props){
 		super(props);
+
+		this.state = {isLoggedIn:true}
 	};
 
 
 	render(){
-		const unreadMessages  = this.props.unreadMessages;
+		const isLoggedIn  = this.state.isLoggedIn;
 		return(
 			<div>
 				<h1>Hello</h1>
-				{unreadMessages.length > 0 && 
-					<h2> You have {unreadMessages.length} unreadMessages.</h2>
-				}
+				The user is <b>{isLoggedIn ? 'currently' : 'not'}</b> logged in.
 			</div>
 		);
 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,9 +7,8 @@ import EventHandling from './components/eventHandling.js';
 import Greeting from './components/Greeting.js';
 import LoginControl from './components/LoginControl.js';
 import MailBox from './components/Mailbox.js';
-
 import registerServiceWorker from './registerServiceWorker';
 
-const messages = ['hi','how', 'are', 'you'];
-ReactDOM.render(<MailBox  unreadMessages = {messages} />, document.getElementById('root'));
+
+ReactDOM.render(<MailBox  />, document.getElementById('root'));
 registerServiceWorker();
