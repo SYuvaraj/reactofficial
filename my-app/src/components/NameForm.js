7da09de493886096d08f3a52d9dbbc080f6e044d
Yuvaraj S <s.yuvaraj03@gmail.com>
2017-08-28 20:45:27 +0530

    controlled com

--- my-app/src/components/NameForm.js
+++ my-app/src/components/NameForm.js
@@ -3,18 +3,25 @@ import React, {Component} from 'react';
 class NameForm extends React.Component{
 	constructor(props){
 		super(props);
-		this.state = {value: ''};
+		this.state = {
+			nameValue: '',
+			textareaVal:'some info'
+		};
 
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
 	handleChange(event){
-		this.setState({value: event.target.value});		
+		this.setState({
+			nameValue: event.target.value,
+			textareaVal: event.target.value
+		});		
 	}
 
 	handleSubmit(event){
-		alert("A name was sumbitted"+this.state.value);
+		alert("A name was sumbitted"+ this.state.value);
+		alert("A textarea was sumbitted"+ this.state.textareaVal);
 		event.preventDefault();
 	}
 	render(){
@@ -22,7 +29,8 @@ class NameForm extends React.Component{
 		return(
 			<form onSubmit = {this.handleSubmit}>
 				<label>
-					Name: <input type="text" value = {this.state.value} onChange  = {this.handleChange} />
+					Name: <input type="text" value = {this.state.nameValue} onChange  = {this.handleChange} />
+					<textarea value = {this.state.textareaVal} onChange = {this.handleChange} />
 				</label>
 				<input type="submit" value="submit" />
 			</form>
