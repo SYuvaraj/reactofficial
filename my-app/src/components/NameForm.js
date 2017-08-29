118ed21366378a2c8098014a951c6f74d3d17b06
Yuvaraj S <s.yuvaraj03@gmail.com>
2017-08-29 16:14:20 +0530

    select

--- my-app/src/components/NameForm.js
+++ my-app/src/components/NameForm.js
@@ -5,11 +5,13 @@ class NameForm extends React.Component{
 		super(props);
 		this.state = {
 			nameValue: '',
-			textareaVal:'some info'
+			textareaVal:'some info',
+			selecVal: 'coconut'
 		};
 
 		this.handleChange = this.handleChange.bind(this);
 		this.handletextAreaChange = this.handletextAreaChange.bind(this);
+		this.handleSelectVal  = this.handleSelectVal.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
@@ -25,9 +27,15 @@ class NameForm extends React.Component{
 		});		
 	}
 
+	handleSelectVal(event){
+		this.setState({
+			selecVal: event.target.value
+		});	
+	}
 	handleSubmit(event){
 		alert("A name was sumbitted "+  this.state.nameValue);
 		alert("A textarea was sumbitted "+ this.state.textareaVal);
+		alert('Your favorite flavor is: ' + this.state.selecVal);
 		event.preventDefault();
 	}
 	render(){
@@ -37,6 +45,12 @@ class NameForm extends React.Component{
 				<label>
 					Name: <input type="text" value = {this.state.nameValue} onChange  = {this.handleChange} />
 					<textarea value = {this.state.textareaVal} onChange = {this.handletextAreaChange} />
+					<select value = {this.state.selecVal} onChange = {this.handleSelectVal}>
+						<option value="grapefruit">Grapefruit</option>
+			            <option value="lime">Lime</option>
+			            <option value="coconut">Coconut</option>
+			            <option value="mango">Mango</option>
+					</select>
 				</label>
 				<input type="submit" value="submit" />
 			</form>
